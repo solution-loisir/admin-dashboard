0b41d05588e4fc21c1bd469f61f3842b1a88755f
Mathieu Huot <mathieu.huot@gmail.com>
2022-12-10 19:56:07 -0500

    Making sure Person.Email isn't null.

--- src/utilities/getEmailList.js
+++ src/utilities/getEmailList.js
@@ -2,7 +2,7 @@ function getEmailList(source) {
   return Object.keys(source).reduce((array, current) => {
     source[current].forEach(activity => {
       activity.Registrations.forEach(registration => {
-        array.push(registration.Person.Email.toLowerCase());
+        if(registration.Person.Email) array.push(registration.Person.Email.toLowerCase());
       });
     });
     return [...new Set(array.sort())];
